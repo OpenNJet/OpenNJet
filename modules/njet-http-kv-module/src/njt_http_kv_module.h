47791b9caae6b7f5801e2155f67195bd1a66f838
Hong Xin <hongxin@asiainfo.com>
2023-02-07 18:06:48 +0800

    add db_kv_get, db_kv_set in kv module

--- modules/njet-http-kv-module/src/njt_http_kv_module.h
+++ modules/njet-http-kv-module/src/njt_http_kv_module.h
@@ -4,5 +4,8 @@
 typedef int (*kv_change_handler)(njt_str_t *key, njt_str_t *value, void *data);
 
 int njt_reg_kv_change_handler(njt_str_t *key, kv_change_handler handler, void *data);
+int njt_db_kv_get(njt_str_t *key, njt_str_t *value);
+int njt_db_kv_set(njt_str_t *key, njt_str_t *value);
+
 
 #endif
