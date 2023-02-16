e4ef75a80e0ad3fbd8718551ef957b89e2e8e09f
libm <libm@tmlake.com>
2023-02-16 16:38:36 +0800

    lua path

--- src/ext/lua/stream/src/njt_stream_lua_util.c
+++ src/ext/lua/stream/src/njt_stream_lua_util.c
@@ -131,7 +131,7 @@ static int njt_stream_lua_req_socket(lua_State *L);
 #endif
 
 
-#if !defined(LUA_DEFAULT_PATH) && (NJT_DEBUG)
+#if !defined(LUA_DEFAULT_PATH) 
 #define LUA_DEFAULT_PATH "/etc/njet/lualib/lib/?.lua;"                      
                          //"/etc/njet/lua-resty-lrucache/lib/?.lua"
 #endif
