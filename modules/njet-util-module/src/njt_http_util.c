73791fd35b8f961dd75a577009b9537a396358f4
程旭 <chengxue@tmlake.com>
2023-02-27 02:42:08 +0000

    fix 头文件引用错误

--- modules/njet-util-module/src/njt_http_util.c
+++ modules/njet-util-module/src/njt_http_util.c
@@ -15,7 +15,7 @@
 //
 
 #include <njt_core.h>
-#include <njt_http.H>
+#include <njt_http.h>
 #include <njt_http_util.h>
 
 njt_http_core_srv_conf_t* njt_http_get_srv_by_port(njt_cycle_t *cycle,njt_pool_t *pool,njt_str_t *addr_port,njt_str_t *server_name){
