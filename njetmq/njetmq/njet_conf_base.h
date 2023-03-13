dfcde50cd119ffea5f51a27883fd3159421ea7db
zyg <272764110@qq.com>
2023-03-13 15:41:53 +0800

    njetmq

--- njetmq/njetmq/njet_conf_base.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CONF_BASE_H_
-#define _CONF_BASE_H_
-
-int conf__parse_bool(char **token, const char *name, bool *value, char *saveptr);
-int conf__parse_int(char **token, const char *name, int *value, char *saveptr);
-int conf__parse_ssize_t(char **token, const char *name, ssize_t *value, char *saveptr);
-int conf__parse_string(char **token, const char *name, char **value, char *saveptr);
-#endif
