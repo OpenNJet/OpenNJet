31674c15b3f15e5d2848cbcf5549d2b0e106aae1
Lubo Chen <chenlb@tmlake.com>
2023-04-03 18:46:14 +0800

    fix json type match

--- modules/njet-util-module/include/njt_json_util.h
+++ modules/njet-util-module/include/njt_json_util.h
@@ -29,11 +29,12 @@ struct njt_json_define_s {
     njt_int_t offset;
     njt_int_t size;
     int8_t type;
+    int8_t eletype;             //just used for array type
     njt_json_define_t *sub;
     njt_parse_item_handler parse;
 };
 
-#define njt_json_define_null {njt_null_string,0,0,NJT_JSON_ERROR,NULL,NULL}
+#define njt_json_define_null {njt_null_string,0,0,NJT_JSON_ERROR,0,NULL,NULL}
 
 njt_int_t njt_json_parse_msec(njt_json_element *el, njt_json_define_t *def, void *data);
 
