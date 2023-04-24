6f6720abcc70eb7d92b6e230015039ab30a7e730
zhaoygf <zhaoygf@tmlake.com>
2023-04-24 15:10:45 +0800

    health_check directive error tips

--- modules/njet-http-upstream-dynamic-servers-module/src/njt_http_upstream_dynamic_servers.c
+++ modules/njet-http-upstream-dynamic-servers-module/src/njt_http_upstream_dynamic_servers.c
@@ -1996,6 +1996,8 @@ static char *njt_http_upstream_check(njt_conf_t *cf, njt_command_t *cmd,
 		    uscf->persistent = 1;
 		    continue;
 		}
+		 njt_conf_log_error(NJT_LOG_EMERG, cf, 0,
+                       "\"%V\" directive is not allowed here", &value[i]);
 		return NJT_CONF_ERROR;
 	}
 	if(uscf->persistent == 1 && uscf->mandatory != 1) {
