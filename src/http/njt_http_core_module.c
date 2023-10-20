0034ed4ce3579afbfa14d09b3dfddefb564c72a6
root <zhaoygf@tmlake.com>
2023-10-20 10:36:47 +0800

    1.server_names 正则bug。 2. location 字段加转义。3.各模块用server full_name

--- src/http/njt_http_core_module.c
+++ src/http/njt_http_core_module.c
@@ -4710,7 +4710,9 @@ njt_http_core_server_name(njt_conf_t *cf, njt_command_t *cmd, void *conf)
         } else {
             sn->name = value[i];
         }
-
+#if (NJT_HTTP_DYNAMIC_LOC) 
+        sn->full_name = sn->name;
+#endif
         if (value[i].data[0] != '~') {
             njt_strlow(sn->name.data, sn->name.data, sn->name.len);
             continue;
@@ -4727,7 +4729,9 @@ njt_http_core_server_name(njt_conf_t *cf, njt_command_t *cmd, void *conf)
                                "empty regex in server name \"%V\"", &value[i]);
             return NJT_CONF_ERROR;
         }
-
+#if (NJT_HTTP_DYNAMIC_LOC) 
+        sn->full_name = sn->name;
+#endif
         value[i].len--;
         value[i].data++;
 
