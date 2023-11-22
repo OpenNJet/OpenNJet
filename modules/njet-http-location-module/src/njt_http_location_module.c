e0bf58ef62703d8e9d79bbfb0ff7c4beef76a170
root <zhaoygf@tmlake.com>
2023-11-22 10:27:57 +0800

    添加错误信息

--- modules/njet-http-location-module/src/njt_http_location_module.c
+++ modules/njet-http-location-module/src/njt_http_location_module.c
@@ -609,11 +609,13 @@ static njt_int_t njt_http_add_location_handler(njt_http_location_info_t *locatio
     if (rv != NULL) {
 	
 		//njt_log_error(NJT_LOG_DEBUG, njt_cycle->log, 0, "njt_conf_parse  location[%V] error:%s",&location_name,rv);
-		//njt_str_set(&location_info->msg,"njt_conf_parse error!");
-		njt_http_location_delete_dyn_var(clcf);
-        njt_http_location_clear_dirty_data(clcf);
-        rc = NJT_ERROR;
-        goto out;
+	    if(location_info->msg.len == NJT_MAX_CONF_ERRSTR && location_info->msg.data[0] == '\0') {
+	    	njt_str_set(&location_info->msg,"njt_conf_parse error!");
+	    }
+	    njt_http_location_delete_dyn_var(clcf);
+	    njt_http_location_clear_dirty_data(clcf);
+	    rc = NJT_ERROR;
+	    goto out;
     }
     njt_log_error(NJT_LOG_DEBUG, njt_cycle->log, 0, "njt_conf_parse end +++++++++++++++");
 
