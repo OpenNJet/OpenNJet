926dec603f079a643149be89750bf1a500d4ee6a
Hong Xin <hongxina@tmlake.com>
2023-12-12 12:21:09 +0800

    run privilege agent as root, and when doing reload, it will start new process after the privious one has completely stop

--- src/os/unix/njt_process_cycle.h
+++ src/os/unix/njt_process_cycle.h
@@ -60,5 +60,6 @@ extern sig_atomic_t    njt_reopen;
 extern sig_atomic_t    njt_change_binary;
 extern sig_atomic_t    njt_reap_helper;
 extern sig_atomic_t    njt_rtc;
-extern njt_uint_t    njt_is_privileged_helper;
+extern njt_uint_t      njt_is_privileged_helper;
+extern njt_uint_t      njt_privileged_agent_exited;
 #endif /* _NJT_PROCESS_CYCLE_H_INCLUDED_ */
