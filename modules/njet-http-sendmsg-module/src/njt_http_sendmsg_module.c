a71eedddfb5bd196eb5b34d416cc347edafbdf74
Hong Xin <hongxina@tmlake.com>
2024-03-21 14:46:34 +0800

    add NULL check in kv api

--- modules/njet-http-sendmsg-module/src/njt_http_sendmsg_module.c
+++ modules/njet-http-sendmsg-module/src/njt_http_sendmsg_module.c
@@ -868,7 +868,7 @@ error:
 int njt_dyn_kv_get(njt_str_t *key, njt_str_t *value)
 {
     uint32_t val_len = 0;
-    if (key->data == NULL)
+    if (key == NULL || key->data == NULL || value == NULL)
     {
         njt_log_error(NJT_LOG_ERR, njt_cycle->log, 0, "njt_dyn_kv_get got wrong key:value data");
         return NJT_ERROR;
