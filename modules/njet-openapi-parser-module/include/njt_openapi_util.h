0dac48e79bed737d854e382a6038affc01f81073
liismn <licmh@tmlake.com>
2024-04-19 13:43:34 +0800

    add openapi utility

---
+++ modules/njet-openapi-parser-module/include/njt_openapi_util.h
@@ -0,0 +1,10 @@
+
+/*
+ * Copyright (C) 2021-2023  TMLake(Beijing) Technology Co., Ltd.
+ */
+
+#include <njt_config.h>
+#include <njt_core.h>
+
+njt_int_t
+njt_openapi_parse_json(njt_str_t *json_str, njt_str_t *db_name, njt_int_t group_id);
