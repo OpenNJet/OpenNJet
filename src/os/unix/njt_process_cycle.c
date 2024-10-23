0b5f46b54094bb8fb08e4b26bae19c5e8a284f98
root <zhaoygf@tmlake.com>
2024-10-23 10:52:05 +0800

    helper 模块删除cache 清理逻辑njt_helper_process_handler,保留定时器njt_time_update

--- src/os/unix/njt_process_cycle.c
+++ src/os/unix/njt_process_cycle.c
@@ -728,8 +728,10 @@ unsigned int njt_helper_check_cmd(void *cctx)
 void
 njt_helper_process_handler(njt_event_t *ev)
 {
-    njt_uint_t    i;
-    njt_msec_t    next = 0, n;
+    //njt_uint_t    i;
+    //njt_msec_t    next = 0, n;
+    njt_msec_t    next = 0;
+    /*
     njt_path_t **path;
 
     path = njt_cycle->paths.elts;
@@ -742,7 +744,8 @@ njt_helper_process_handler(njt_event_t *ev)
 
             njt_time_update();
         }
-    }
+    }*/
+    njt_time_update();
 
     if (next == 0) {
         next = 1;
@@ -895,7 +898,7 @@ njt_helper_process_cycle(njt_cycle_t *cycle, void *data)
     njt_memcpy(title + len, ctx->param.conf_fn.data, len2);
     title[len + len2] = 0;
     njt_setproctitle(title);
-    //njt_add_timer(&ev, 0); by zyg
+    njt_add_timer(&ev, 0);
     ctx->param.check_cmd_fp = njt_helper_check_cmd;
     ctx->param.ctx = cycle;
 
