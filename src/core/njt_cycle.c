a045f8fde2a82a2e5d89bca3a4092a03fee9f614
liismn <licmh@tmlake.com>
2024-12-23 11:48:35 +0800

    dyn_slab: make ASAN clean on reload

--- src/core/njt_cycle.c
+++ src/core/njt_cycle.c
@@ -331,7 +331,9 @@ njt_init_cycle(njt_cycle_t *old_cycle)
 #endif
 
 
-    njt_share_slab_set_init_phase(cycle); // for dyn shm apply in post config
+    if (njt_process != NJT_PROCESS_HELPER) {
+        njt_share_slab_set_init_phase(cycle); // for dyn shm apply in post config
+    }
     if (njt_is_init_cycle(old_cycle)) {
         njt_share_slab_create_hidden_dir(cycle);
     }
