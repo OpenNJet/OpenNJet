da418b3cbd7e61f3f27299f4b0f449deee4fd59e
root <zhaoygf@tmlake.com>
2025-05-08 17:23:03 +0800

    释放内存

--- modules/njet-http-kv-module/src/njt_http_kv_module.c
+++ modules/njet-http-kv-module/src/njt_http_kv_module.c
@@ -1000,9 +1000,9 @@ njt_int_t njt_http_kv_update_fullconfig(njt_str_t *key)
 
                 njt_kv_sendmsg(&send_topic, &full_conf, 1);
                 njt_free(full_conf.data);
-                return NJT_OK;
             }
-	     njt_free(send_topic.data);
+	      njt_free(send_topic.data);
+          return NJT_OK;
         }
     }
     return NJT_ERROR;
