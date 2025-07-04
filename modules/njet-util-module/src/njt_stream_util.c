2acc1ce7906f5961519feebef7cbeb3ab4330bfd
root <zhaoygf@tmlake.com>
2025-07-04 09:58:17 +0800

    njt_stream_get_listens_by_server

--- modules/njet-util-module/src/njt_stream_util.c
+++ modules/njet-util-module/src/njt_stream_util.c
@@ -6,42 +6,140 @@
 #include <njt_stream.h>
 extern njt_str_t njt_get_command_unique_name(njt_pool_t *pool,njt_str_t src);
 
-njt_int_t njt_stream_get_listens_by_server(njt_array_t *array,
-    njt_stream_core_srv_conf_t *cscf,
-    njt_stream_core_main_conf_t *cmcf)
-{
-    njt_uint_t               i, j;
-    njt_stream_conf_port_t  *ports, *port;
-    njt_stream_conf_addr_t  *addr;
-    njt_str_t               *listen;
+// 获取server的listen 字符串列表
+njt_int_t njt_stream_get_listens_by_server(njt_array_t *array,njt_stream_core_srv_conf_t  *cscf){
+//    njt_hash_elt_t  **elt;
+	njt_listening_t *ls;
+	njt_uint_t i,j,k,len;
+	njt_stream_port_t *port;
+	njt_stream_in_addr_t *addr;
+	njt_uint_t         worker;
+	njt_stream_in6_addr_t *addr6;
+	njt_stream_addr_conf_t             *addr_conf;   //(njt_process == NJT_PROCESS_HELPER && njt_is_privileged_agent)  if (!ls[i].reuseport || ls[i].worker != 0)
+	njt_str_t *listen, data;
+	njt_stream_server_name_t *sn;
+	njt_stream_core_srv_conf_t  *tcscf;
+	struct sockaddr          *sockaddr;
+	struct sockaddr_in6  *sin6, ssin6;
+	struct sockaddr_in   *sin,   ssin;
+	in_port_t  nport;
+	//njt_uint_t  addr_len = NJT_SOCKADDR_STRLEN;
+	worker = njt_worker;
+	if (njt_process == NJT_PROCESS_HELPER && njt_is_privileged_agent) {
+		worker = 0;
+	}
+	ls = njt_cycle->listening.elts;
+	for (i = 0; i < njt_cycle->listening.nelts; ++i) {
+		if(ls[i].server_type != NJT_STREAM_SERVER_TYPE){
+			continue; // 非stream listen
+		}
+		if (ls[i].reuseport && ls[i].worker != worker) {
+			continue; 
+		}
+		port = ls[i].servers;
+		addr=NULL;
+		addr6=NULL;
+		switch (ls[i].sockaddr->sa_family) {
 
-    if (cmcf == NULL || cmcf->ports == NULL) {
-        return NJT_OK;
-    }
+#if (NJT_HAVE_INET6)
+			case AF_INET6:
+				addr6 = port->addrs;
+				break;
+#endif
+			default: /* AF_INET */
+				addr = port->addrs;
+				break;
+		}
 
-    ports = cmcf->ports->elts;
+		for (j = 0; j < port->naddrs ; ++j) {
+			if(addr6 != NULL){
+				addr_conf = &addr6[j].conf;
+			}else{
+				addr_conf = &addr[j].conf;
+			}
+			if(addr_conf == NULL){
+				continue;
+			}
+			listen = NULL;
+			if(addr_conf->default_server == cscf ){
+				listen  = njt_array_push(array);
+				if(listen == NULL){
+					return NJT_ERROR_ERR;
+				}
+			}
+			if (listen == NULL && addr_conf->virtual_names != NULL) {
+				if(cscf->server_name.len > 0 && cscf->server_name.data[0] == '~' ) {
+					sn = addr_conf->virtual_names->regex;
+					for (k = 0; k <  addr_conf->virtual_names->nregex; ++k) {
+						if(sn[k].server == cscf){
+							listen  = njt_array_push(array);
+							if(listen == NULL){
+								return NJT_ERROR_ERR;
+							}
+							break;
+						}
+					}
+				} else {
+					tcscf = njt_hash_find_combined(&addr_conf->virtual_names->names,
+							njt_hash_key(cscf->server_name.data, cscf->server_name.len),
+							cscf->server_name.data, cscf->server_name.len);
+					if(cscf == tcscf){
+						listen  = njt_array_push(array);
+						if(listen == NULL){
+							return NJT_ERROR_ERR;
+						}
+					}
+				} 
 
-    for (i = 0; i < cmcf->ports->nelts; i++) {
+			}
+			if(listen != NULL) {
 
-        port = &ports[i];
-        addr = port->addrs.elts;
+				if (ls[i].sockaddr->sa_family == AF_UNIX) {
+					*listen = ls[i].addr_text;
+				} else {
+					nport = njt_inet_get_port(ls[i].sockaddr);
+					data.data = njt_pnalloc(array->pool, NJT_SOCKADDR_STRLEN);
+					if (data.data == NULL) {
+						return NJT_ERROR_ERR;
+					}
+					data.len = NJT_SOCKADDR_STRLEN;
 
-        for (j = 0; j < port->addrs.nelts; j++) {
-            listen = NULL;
-            if ((njt_stream_core_srv_conf_t *)addr[j].default_server == cscf) {
-                listen = njt_array_push(array);
-                if (listen == NULL) {
-                    return NJT_ERROR_ERR;
-                }
-                *listen = addr[j].opt.addr_text;
-            }
-        }
-    }
-    return NJT_OK;
+					//njt_memzero(&sockaddr,sizeof(struct sockaddr ));
+					//sockaddr.sa_family = ls[i].sockaddr->sa_family;
+					sockaddr = NULL;
+					if(ls[i].sockaddr->sa_family == AF_INET6) {
+						sockaddr = (struct sockaddr *)&ssin6;
+						njt_memzero(&ssin6,sizeof(struct sockaddr_in6 ));
+						sin6 = (struct sockaddr_in6  *)sockaddr;  //if (njt_memcmp(&addr6[i].addr6, &ssin6->sin6_addr,16)
+						njt_memcpy(&sin6->sin6_addr, &addr6[j].addr6, 16);
+					} else if (ls[i].sockaddr->sa_family == AF_INET)  {
+						sockaddr = (struct sockaddr *)&ssin;
+						njt_memzero(&ssin,sizeof(struct sockaddr_in ));
+						sin = (struct sockaddr_in  *)sockaddr;
+						sin->sin_addr.s_addr = addr[j].addr;
+					}
+					if (sockaddr == NULL) {
+						return NJT_ERROR;
+					} 
+					sockaddr->sa_family = ls[i].sockaddr->sa_family;
+					njt_inet_set_port(sockaddr,nport);
+					len = njt_sock_ntop(sockaddr, sizeof(njt_sockaddr_t),
+							data.data, data.len, 1);
+					if (len == 0) {
+						return NJT_ERROR;
+					}
+					data.len = len;
+					*listen = data;
+				}
+
+			}
+		}
+	}
+	return NJT_OK;
 }
 
 
-njt_stream_core_srv_conf_t* njt_stream_get_srv_by_port(njt_cycle_t *cycle,njt_str_t *addr_port,njt_str_t *server_name){
+njt_stream_core_srv_conf_t* njt_stream_get_srv_by_server_name(njt_cycle_t *cycle,njt_str_t *addr_port,njt_str_t *server_name){
 	njt_stream_core_srv_conf_t* cscf, *ret_cscf;
 	njt_listening_t *ls, *target_ls = NULL;
 	njt_uint_t i,j,k;
@@ -100,7 +198,7 @@ njt_stream_core_srv_conf_t* njt_stream_get_srv_by_port(njt_cycle_t *cycle,njt_st
 		ls = cycle->listening.elts;
 		for (i = 0; i < cycle->listening.nelts; i++) {
 			if(ls[i].server_type != NJT_STREAM_SERVER_TYPE){
-				continue; // 非http listen
+				continue; // 非stream listen
 			}
 			if (ls[i].reuseport && ls[i].worker != worker) {
 				continue; 
@@ -202,7 +300,7 @@ out:
 	return ret_cscf;
 }
 
-njt_stream_core_srv_conf_t* njt_stream_get_srv_by_server_name(njt_cycle_t *cycle,njt_str_t *addr_port,njt_str_t *server_name){
+njt_stream_core_srv_conf_t* njt_stream_get_srv_by_port(njt_cycle_t *cycle,njt_str_t *addr_port,njt_str_t *server_name){
 
 	njt_pool_t  *pool;
 	njt_str_t   name;
@@ -214,7 +312,7 @@ njt_stream_core_srv_conf_t* njt_stream_get_srv_by_server_name(njt_cycle_t *cycle
 		return NULL;
 	}
 	name = njt_get_command_unique_name(pool,*server_name);
-	srv = njt_stream_get_srv_by_port(cycle,addr_port,&name);
+	srv = njt_stream_get_srv_by_server_name(cycle,addr_port,&name); 
 
 	njt_destroy_pool(pool);
 	return srv;
