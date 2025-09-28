3070200488d216534c466af020fec50385d1de2c
Lubo Chen <chenlb@tmlake.com>
2025-09-28 13:50:52 +0800

    recover multi header

---
+++ modules/njet-http-multi-header-module/src/njt_http_multi_header_module.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) Igor Sysoev
+ * Copyright (C) Nginx, Inc.
+ * Copyright (C) 2021-2023  TMLake(Beijing) Technology Co., Ltd.
+ */
+#ifndef NJT_HTTP_MULTI_HEADER_MODULE_H_
+#define NJT_HTTP_MULTI_HEADER_MODULE_H_
+#include <njt_config.h>
+#include <njt_core.h>
+#include <njt_http.h>
+
+
+#endif
